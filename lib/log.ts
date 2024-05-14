d1f8d7e06edaac12a5b8f9cf37b2e73cf735322e
curtwphillips <curtwphillips@gmail.com>
2024-05-14 00:49:53 -0500

    update log to allow local time

--- lib/log.ts
+++ lib/log.ts
@@ -3,6 +3,7 @@ import { stringify } from './objects';
 type Config = {
   hideFile: boolean;
   hideTime: boolean;
+  useLocalTime: boolean;
 };
 
 export const getFileFromStack = (stack) => stack[0].getFileName();
@@ -10,7 +11,7 @@ export const getLineFromStack = (stack) => stack[0].getLineNumber();
 
 // log the filename, line number, and auto stringify objects
 export const log =
-  (config: Config = { hideFile: false, hideTime: false }) =>
+  (config: Config = { hideFile: false, hideTime: false, useLocalTime: true }) =>
   (...args) => {
     const orig = Error.prepareStackTrace;
     Error.prepareStackTrace = function (_, stack) {
@@ -27,7 +28,13 @@ export const log =
       args.unshift(file + ':' + getLineFromStack(stack));
     }
     if (!config.hideTime) {
-      args.unshift(new Date().toISOString());
+      if (config.useLocalTime) {
+        const offsetMs = new Date().getTimezoneOffset() * 60000;
+        const localISOTime = new Date(Date.now() - offsetMs).toISOString().slice(0, -1);
+        args.unshift(localISOTime);
+      } else {
+        args.unshift(new Date().toISOString());
+      }
     }
     args.forEach((arg) => {
       const fixedArg = typeof arg === 'object' ? JSON.parse(stringify(arg)) : arg;
